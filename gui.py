b875eca396b368768f8d9e27de559677e7b1c216
ocelot <probablyanocelot@gmail.com>
2022-10-25 06:43:42 -0500

    template based on lottery_stats

---
+++ gui.py
@@ -0,0 +1,80 @@
+from kivy.app import App
+from kivy.core.window import Window
+from kivy.uix.widget import Widget
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+from kivy.uix.popup import Popup
+
+Window.clearcolor = .3, .3, .3, 1
+
+'''
+Always update here if change .kv
+'''
+T0_SCREENS = ['settings', 'input', 'pick_menu']
+T1_SCREENS = ['delete_sheet']
+
+
+screen_tier = 0
+
+
+class gui(App):
+    def __init__(self, **kwargs):
+        super(gui, self).__init__(**kwargs)
+
+        # establish previous_screen variable to be used in the back button
+        self.previous_screen = ""
+
+    def build(self):
+        return ScreenManagement()
+
+
+class ScreenManagement(ScreenManager):
+    pass
+
+
+'''
+        ----    SCREENS     ----
+'''
+
+
+class HomeScreen(Screen):
+    def __init__(self, **kwargs):
+        super(Screen, self).__init__(**kwargs)
+
+    # def refresh_datasets(self):
+    #     downloader.master()
+
+
+'''
+        ----    BUTTONS     ----
+'''
+
+
+class HomeButton(Button):
+    def __init__(self, **kwargs):
+        super(HomeButton, self).__init__(**kwargs)
+        # self.add_widget(Label(text='Back', on_press=self.on_press))
+
+    def on_press(self):
+        App.get_running_app().root.current = 'home'
+
+
+class BackButton(Button):
+    def __init__(self, **kwargs):
+        super(BackButton, self).__init__(**kwargs)
+    # back = ObjectProperty()
+
+    def on_press(self):
+        if App.get_running_app().root.current in tuple(T1_SCREENS):
+            App.get_running_app().root.current = App.get_running_app().root.previous()
+        else:
+            App.get_running_app().root.current = 'home'
+
+
+if __name__ == '__main__':
+    gui().run()
